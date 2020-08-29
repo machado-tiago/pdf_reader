0638c8aeaaa02f3d414e3d7f1e06d55384493aaf
machado-tiago <tiagomachadox@gmail.com>
2020-08-29 07:28:20 -0300

    versao da noite

--- pdf_reader.py
+++ pdf_reader.py
@@ -2,14 +2,27 @@ import pandas as pd
 import os
 import pathlib
 import tabula
-import camelot
+#import camelot
+import PyPDF2
 
 pasta = pathlib.Path('D:\Tiago\Documentos\IRPF\IRPF2021')
 arquivo=os.path.join(pasta, '2020.08-37984_NotaCorretagem.pdf')
-df=tabula.read_pdf(arquivo,pages="all",multiple_tables=True)
-print(df)
+lista=tabula.read_pdf(arquivo,pages="all",multiple_tables=True)
 
+#print(df)
+for i,tabela in enumerate(lista):
+    tb = pd.DataFrame(tabela)
+    print(tb)
+    dataPregao = tb.loc[0,'Data Pregão']
+    print(dataPregao)
+    
+
+#texto=PyPDF2.PdfFileReader(arquivo)
+#n_paginas = texto.getNumPages()
+
+'''
 tabelas= camelot.read_pdf(arquivo, pages="all")
 for i,tabela in enumerate(tabelas):
     tb = tabela.df
-    print(tb)
+#    print(tb)
+'''
